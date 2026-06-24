2e9bbc5549e4f0ad4d3fae41c6b915336f6d3c9c
Mita Anggraeni <mitaanggraeni733@gmail.com>
2026-06-24 23:27:00 +0700

    add repository

---
+++ lib/features/home/data/domain/news_repository.dart
@@ -0,0 +1,40 @@
+import 'package:dio/dio.dart';
+import '../../../../core/network/dio_client.dart';
+
+class Article {
+  final String title;
+  final String description;
+
+  Article({required this.title, required this.description});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'] ?? 'No Title',
+      description: json['description'] ?? '',
+    );
+  }
+}
+
+class NewsRepository {
+  final DioClient dioClient;
+
+  NewsRepository(this.dioClient);
+
+  Future<List<Article>> fetchTopNews() async {
+    try {
+      final response = await dioClient.dio.get(
+        'top-headlines',
+        queryParameters: {'category': 'technology', 'language': 'en'},
+      );
+
+      final List<dynamic> articlesJson = response.data['articles'];
+      List<Article> articles = articlesJson.map((json) => Article.fromJson(json)).toList();
+
+      articles.sort((a, b) => b.title.toLowerCase().compareTo(a.title.toLowerCase()));
+
+      return articles;
+    } catch (e) {
+      throw Exception('Gagal mengambil data berita: $e');
+    }
+  }
+}
